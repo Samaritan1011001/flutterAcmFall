3c1f4ad3b53a4d77be2922d790d66f579fae7b34
Manoj <manojnb95@gmail.com>
2020-10-07 20:37:16 -0500

    Initial Commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          height: 70,
+          color: Colors.teal,
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.list)),
+              Tab(icon: Icon(Icons.schedule)),
+            ],
+            indicatorColor: Colors.white,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.shopping_basket),
+            Icon(Icons.directions_transit),
+          ],
+        ),
+
+      ),
+    );
+  }
+}
