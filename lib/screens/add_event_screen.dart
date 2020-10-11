0b0871dc13714ae7b423b788696c5c9484166daa
b72u68 <mdinh@hawk.iit.edu>
2020-10-11 14:14:59 -0500

    Added animation to event screen

--- lib/screens/add_event_screen.dart
+++ lib/screens/add_event_screen.dart
@@ -12,13 +12,17 @@ class _AddEventScreen extends State<AddEventScreen> {
   Map _event = {};
 
   bool _openMoreSetting = false;
+
   bool _isDone = false;
+
   String _eventTitle;
   String _eventNote;
+
   bool _setDate = false;
-  DateTime _eventDate = DateTime.now();
+  DateTime _eventDate;
+
   bool _setTime = false;
-  DateTime _eventTime = DateTime.now();
+  DateTime _eventTime;
 
   void _onChangeEventTitle(String eventTitle) {
     setState(() {
@@ -35,6 +39,11 @@ class _AddEventScreen extends State<AddEventScreen> {
   void _toggleDate() {
     setState(() {
       _setDate = !_setDate;
+      if (!_setDate) {
+        _setTime = false;
+        _eventDate = null;
+        _eventTime = null;
+      }
     });
   }
 
@@ -47,6 +56,9 @@ class _AddEventScreen extends State<AddEventScreen> {
   void _toggleTime() {
     setState(() {
       _setTime = !_setTime;
+      if (!_setTime) {
+        _eventTime = null;
+      }
     });
   }
 
@@ -56,10 +68,10 @@ class _AddEventScreen extends State<AddEventScreen> {
     });
   }
 
-  void _closeMoreSetting() {
+  void _toggleMoreSetting() {
     FocusScope.of(context).requestFocus(new FocusNode());
     setState(() {
-      _openMoreSetting = false;
+      _openMoreSetting = !_openMoreSetting;
     });
   }
 
@@ -99,12 +111,12 @@ class _AddEventScreen extends State<AddEventScreen> {
                 InkWell(
                     onTap: () {
                       FocusScope.of(context).requestFocus(new FocusNode());
-                      String dateFormat = _setDate
+                      String dateFormat = _eventDate != null
                           ? '${_eventDate.month}-${_eventDate.day}-${_eventDate.year}'
-                          : '';
-                      String timeFormat = _setTime
+                          : null;
+                      String timeFormat = _eventTime != null
                           ? '${_eventTime.hour}:${_eventTime.minute}'
-                          : '';
+                          : null;
                       _event = {
                         "eventTitle": _eventTitle,
                         "eventNote": _eventNote,
@@ -164,12 +176,7 @@ class _AddEventScreen extends State<AddEventScreen> {
                     )),
                     // More info button: add date and time
                     InkWell(
-                        onTap: () {
-                          FocusScope.of(context).requestFocus(new FocusNode());
-                          setState(() {
-                            _openMoreSetting = true;
-                          });
-                        },
+                        onTap: _toggleMoreSetting,
                         highlightColor: Colors.transparent,
                         splashColor: Colors.transparent,
                         child: Padding(
@@ -180,26 +187,25 @@ class _AddEventScreen extends State<AddEventScreen> {
         ]),
         _openMoreSetting
             ? AnimatedContainer(
+                curve: Curves.fastLinearToSlowEaseIn,
                 duration: Duration(milliseconds: 1000),
                 color: Color.fromRGBO(100, 100, 100, 80))
             : Container(),
-        _openMoreSetting
-            ? MoreEventSetting(
-                eventTitle: _eventTitle,
-                note: _eventNote,
-                isOpen: _openMoreSetting,
-                eventDate: _eventDate,
-                setDate: _setDate,
-                eventTime: _eventTime,
-                setTime: _setTime,
-                onChangeNote: _onChangeNote,
-                toggleDate: _toggleDate,
-                onChangeDate: _onChangeDate,
-                toggleTime: _toggleTime,
-                onChangeTime: _onChangeTime,
-                closeSetting: _closeMoreSetting,
-              )
-            : Container(),
+        MoreEventSetting(
+          eventTitle: _eventTitle,
+          note: _eventNote,
+          isOpen: _openMoreSetting,
+          eventDate: _eventDate,
+          setDate: _setDate,
+          eventTime: _eventTime,
+          setTime: _setTime,
+          onChangeNote: _onChangeNote,
+          toggleDate: _toggleDate,
+          onChangeDate: _onChangeDate,
+          toggleTime: _toggleTime,
+          onChangeTime: _onChangeTime,
+          closeSetting: _toggleMoreSetting,
+        ),
       ]),
     )));
   }
@@ -241,150 +247,158 @@ class MoreEventSetting extends StatefulWidget {
 }
 
 class _MoreEventSetting extends State<MoreEventSetting> {
+  double _settingYOffSet;
+
   @override
   Widget build(BuildContext context) {
-    double _settingYOffSet = widget.isOpen ? 30 : 0;
+    _settingYOffSet = widget.isOpen ? 30 : MediaQuery.of(context).size.height;
     String _eventTitle = (widget.eventTitle != null && widget.eventTitle != '')
         ? ((widget.eventTitle.length >= 15)
             ? '${widget.eventTitle.substring(0, 15)}...'
             : widget.eventTitle)
         : 'New Event';
 
-    return GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () {
-          FocusScope.of(context).requestFocus(new FocusNode());
-        },
-        child: AnimatedContainer(
-            curve: Curves.fastLinearToSlowEaseIn,
-            duration: Duration(milliseconds: 1000),
-            transform: Matrix4.translationValues(0, _settingYOffSet, 1),
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
+    return AnimatedContainer(
+        curve: Curves.fastLinearToSlowEaseIn,
+        duration: Duration(milliseconds: 1000),
+        transform: Matrix4.translationValues(0, _settingYOffSet, 1),
+        decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25.0),
+              topRight: Radius.circular(25.0),
+            )),
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: Column(children: <Widget>[
+            Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    )),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                          child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 20.0),
+                              child: Text(_eventTitle,
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold)))),
+                      InkWell(
+                          onTap: widget.closeSetting,
+                          highlightColor: Colors.transparent,
+                          splashColor: Colors.transparent,
+                          child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 20.0),
+                              child: Text("Done",
+                                  style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 23,
+                                      fontWeight: FontWeight.bold)))),
+                    ])),
+            Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Container(
+                    color: Colors.white,
+                    child: TextFormField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 3,
+                      initialValue: widget.note,
+                      onChanged: widget.onChangeNote,
+                      decoration: InputDecoration(
+                          hintText: "Notes",
+                          hintStyle:
+                              TextStyle(fontSize: 20, color: Colors.grey),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 25.0, vertical: 10.0)),
+                      style: TextStyle(fontSize: 21, color: Colors.black),
+                    ))),
+            Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Container(
+                  color: Colors.white,
+                  child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text("Set date reminder",
+                                style: TextStyle(fontSize: 21)),
+                            InkWell(
+                                onTap: widget.toggleDate,
+                                highlightColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                child: Icon(
+                                    (widget.setDate
+                                        ? Icons.toggle_on
+                                        : Icons.toggle_off),
+                                    color: (widget.setDate
+                                        ? Colors.green
+                                        : Colors.grey),
+                                    size: 65)),
+                          ])),
                 )),
-            child: Column(children: <Widget>[
-              Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20.0),
-                        topRight: Radius.circular(20.0),
-                      )),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                            child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 20.0, vertical: 20.0),
-                                child: Text(_eventTitle,
-                                    style: TextStyle(
-                                        fontSize: 30,
-                                        fontWeight: FontWeight.bold)))),
-                        InkWell(
-                            onTap: widget.closeSetting,
-                            highlightColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 20.0, vertical: 20.0),
-                                child: Text("Done",
-                                    style: TextStyle(
-                                        color: Colors.blue,
-                                        fontSize: 23,
-                                        fontWeight: FontWeight.bold)))),
-                      ])),
-              Column(children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Container(
+            widget.setDate
+                ? Column(children: <Widget>[
+                    Container(
                         color: Colors.white,
-                        child: TextFormField(
-                          keyboardType: TextInputType.multiline,
-                          maxLines: 3,
-                          initialValue: widget.note,
-                          onChanged: widget.onChangeNote,
-                          decoration: InputDecoration(
-                              hintText: "Notes",
-                              hintStyle:
-                                  TextStyle(fontSize: 21, color: Colors.grey),
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 25.0, vertical: 10.0)),
-                          style: TextStyle(fontSize: 21, color: Colors.black),
-                        ))),
-                Padding(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Container(
-                      color: Colors.white,
-                      child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 25.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text("Set date reminder",
-                                    style: TextStyle(fontSize: 21)),
-                                InkWell(
-                                    onTap: widget.toggleDate,
-                                    highlightColor: Colors.transparent,
-                                    splashColor: Colors.transparent,
-                                    child: Icon(
-                                        (widget.setDate
-                                            ? Icons.toggle_on
-                                            : Icons.toggle_off),
-                                        color: (widget.setDate
-                                            ? Colors.green
-                                            : Colors.grey),
-                                        size: 65)),
-                              ])),
-                    )),
-                widget.setDate
-                    ? Container(
-                        color: Colors.white,
-                        height: 130,
+                        height: widget.setDate ? 130 : 0,
                         child: CupertinoDatePicker(
-                            initialDateTime: widget.eventDate,
+                            initialDateTime: widget.eventDate != null
+                                ? widget.eventDate
+                                : DateTime.now(),
                             mode: CupertinoDatePickerMode.date,
-                            onDateTimeChanged: widget.onChangeDate))
-                    : Container(),
-                Padding(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Container(
-                      color: Colors.white,
-                      child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 25.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text("Set time reminder",
-                                    style: TextStyle(fontSize: 21)),
-                                InkWell(
-                                    onTap: widget.toggleTime,
-                                    highlightColor: Colors.transparent,
-                                    splashColor: Colors.transparent,
-                                    child: Icon(
-                                        (widget.setTime
-                                            ? Icons.toggle_on
-                                            : Icons.toggle_off),
-                                        color: (widget.setTime
-                                            ? Colors.green
-                                            : Colors.grey),
-                                        size: 65)),
-                              ])),
-                    )),
-                widget.setTime
-                    ? Container(
-                        color: Colors.white,
-                        height: 130,
-                        child: CupertinoDatePicker(
-                            initialDateTime: widget.eventTime,
-                            mode: CupertinoDatePickerMode.time,
-                            onDateTimeChanged: widget.onChangeTime))
-                    : Container(),
-              ]),
-            ])));
+                            onDateTimeChanged: widget.onChangeDate)),
+                    Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Container(
+                          color: Colors.white,
+                          child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 25.0),
+                              child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text("Set time reminder",
+                                        style: TextStyle(fontSize: 21)),
+                                    InkWell(
+                                        onTap: widget.toggleTime,
+                                        highlightColor: Colors.transparent,
+                                        splashColor: Colors.transparent,
+                                        child: Icon(
+                                            (widget.setTime
+                                                ? Icons.toggle_on
+                                                : Icons.toggle_off),
+                                            color: (widget.setTime
+                                                ? Colors.green
+                                                : Colors.grey),
+                                            size: 65)),
+                                  ])),
+                        )),
+                    widget.setTime
+                        ? Container(
+                            color: Colors.white,
+                            height: 130,
+                            child: CupertinoDatePicker(
+                                initialDateTime: widget.eventTime != null
+                                    ? widget.eventTime
+                                    : DateTime.now(),
+                                mode: CupertinoDatePickerMode.time,
+                                onDateTimeChanged: widget.onChangeTime))
+                        : Container(),
+                  ])
+                : Container(),
+          ]),
+        ));
   }
 }
