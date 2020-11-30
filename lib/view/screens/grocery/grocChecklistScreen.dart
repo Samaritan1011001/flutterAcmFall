e8aef4f501a864f7c47d8db03031fec5a580f06e
bisma99 <s.bismaali@gmail.com>
2020-11-30 10:16:33 -0600

    see list on other screen

--- lib/view/screens/grocery/grocChecklistScreen.dart
+++ lib/view/screens/grocery/grocChecklistScreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutterAcmFall/model/objects/User.dart';
 import 'package:flutterAcmFall/view/screens/grocery/checklistCreationScreen.dart';
@@ -6,32 +5,64 @@ import 'package:flutterAcmFall/view/screens/grocery/my_grocery_lists.dart';
 import 'package:flutterAcmFall/view/widget/logout_button.dart';
 import 'package:intl/intl.dart';
 
-class ChecklistScreen extends StatefulWidget{
+class ChecklistScreen extends StatefulWidget {
   _ChecklistScreen createState() => _ChecklistScreen();
 }
 
-class _ChecklistScreen extends State<ChecklistScreen>{
+class _ChecklistScreen extends State<ChecklistScreen> {
   User _user = User(id: "1234567", group: " ");
 
-  Grocery _cardGrocery =
-  Grocery(checklist: null, isDone: false, user: null);
-
-
-
+  Grocery _cardGrocery = Grocery(checklist: null, isDone: false, user: null);
 
   List<Grocery> grocery = [
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Milk")], date: DateTime.now()), isDone: false, user: User(id:"A1", group:" ") ),
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Bread")], date: DateTime.now()), isDone: false, user: User(id:"A2", group:" ")),
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Eggs")], date: DateTime.now()),  isDone: false, user: User(id:"A3", group:" ")),
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Chocolate")], date: DateTime.now()),  isDone: false, user: User(id:"A4", group:" ") ),
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Ice cream")], date: DateTime.now()), isDone: false, user: User(id:"A5", group:" ")),
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Chips")], date: DateTime.now()), isDone: false, user: User(id:"A6", group:" ")),
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Drinks")], date: DateTime.now()), isDone: false, user: User(id:"A1", group:" ") ),
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Water")], date: DateTime.now()), isDone: false, user: User(id:"A2", group:" ")),
-    Grocery(checklist:ChecklistModel(items:[ChecklistItemModel(text:"Onions")], date: DateTime.now()), isDone: false, user: User(id:"A3", group:" ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Milk")], date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A1", group: " ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Bread")], date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A2", group: " ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Eggs")], date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A3", group: " ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Chocolate")],
+            date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A4", group: " ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Ice cream")],
+            date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A5", group: " ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Chips")], date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A6", group: " ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Drinks")], date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A1", group: " ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Water")], date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A2", group: " ")),
+    Grocery(
+        checklist: ChecklistModel(
+            items: [ChecklistItemModel(text: "Onions")], date: DateTime.now()),
+        isDone: false,
+        user: User(id: "A3", group: " ")),
   ];
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -42,74 +73,76 @@ class _ChecklistScreen extends State<ChecklistScreen>{
       }
 
     }*/
-   return Scaffold(
-     backgroundColor: Colors.white,
-
-
-     appBar: AppBar(
-         toolbarHeight: 80,
-         backgroundColor: Colors.white,
-         shadowColor: Colors.transparent,
-         leading: LogoutButton(),
-         title: Text("Group Grocery List",
-             style: TextStyle(
-                 color: Colors.black,
-                 fontWeight: FontWeight.bold,
-                 fontSize: 30)),
-         actions: <Widget>[
-           Padding(
-             padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 30.0),
-           ),
-
-         ]),
-
-     body:
-     Column(
-         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-         children: <Widget> [
-         Container(
-             height: MediaQuery.of(context).size.height - 130 - 112 - 70,
-
-
-          child: ListView.builder(
-
-              itemCount: grocery.length,
-           itemBuilder: (context, index) {
-             return CheckboxListTile(
-               title: Text(grocery[index].checklist.items[0].text, style: TextStyle(fontFamily: 'SFProText', fontSize: 18)),
-               subtitle: Text(DateFormat('MM/dd/yyyy – kk:mm').format(grocery[index].checklist.date), style: TextStyle(fontFamily: 'SFProText', fontSize: 14)),
-               value: grocery[index].isDone,
-               controlAffinity: ListTileControlAffinity.leading,
-
-
-               onChanged: (bool value) {
-                 setState(() {
-                   grocery[index].isDone = value;
-                 });
-               },
-             );
-           })),
-
-      Padding(
-         padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-        child: InkWell(
-          onTap: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyGroceryListsScreen()));},
-          highlightColor: Colors.transparent,
-          splashColor: Colors.transparent,
-           child:  Container(
-                      height:100,
-                       decoration: BoxDecoration(
-                         color: Colors.red,
-                         borderRadius: BorderRadius.circular(10)
-                     ),
-                     child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: <Widget>[
-                           Text("My Grocery List", style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.white)),
-                            /*userGrocCount > 0
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            toolbarHeight: 80,
+            backgroundColor: Colors.white,
+            shadowColor: Colors.transparent,
+            leading: LogoutButton(),
+            title: Text("Group Grocery List",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30)),
+            actions: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 30.0),
+              ),
+            ]),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                  height: MediaQuery.of(context).size.height - 130 - 112 - 70,
+                  child: ListView.builder(
+                      itemCount: grocery.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Checkbox(
+                            value: grocery[index].isDone,
+                            onChanged: (bool value) {
+                              setState(() {
+                                grocery[index].isDone = value;
+                              });
+                            },
+                          ),
+                          title: Text(grocery[index].checklist.items[0].text,
+                              style: TextStyle(
+                                  fontFamily: 'SFProText', fontSize: 18)),
+                          subtitle: Text(
+                              DateFormat('MM/dd/yyyy – kk:mm')
+                                  .format(grocery[index].checklist.date),
+                              style: TextStyle(
+                                  fontFamily: 'SFProText', fontSize: 14)),
+                        );
+                      })),
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => MyGroceryListsScreen()));
+                    },
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 40),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: <Widget>[
+                                Text("My Grocery List",
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                                /*userGrocCount > 0
                                 ? Text(
                                 "$userGrocCount " +
                                     (userGrocCount == 1
@@ -120,28 +153,21 @@ class _ChecklistScreen extends State<ChecklistScreen>{
                                     color: Color.fromRGBO(
                                         244, 244, 244, 0.5)))
                                 : Container()*/
-
-
-                   ])),
-                ),
-                   ))]
-     ));
+                              ])),
+                    ),
+                  ))
+            ]));
   }
+}
 
+class Grocery {
+  ChecklistModel checklist;
+  bool isDone;
+  User user;
+
+  Grocery({this.checklist, this.isDone, this.user});
+
+  String toString() {
+    return '{checklist: ${this.checklist}, isDone: ${this.isDone}, user: $user}';
   }
-
-  class Grocery{
-    ChecklistModel checklist;
-    bool isDone;
-    User user;
-
-    Grocery({this.checklist, this.isDone, this.user});
-
-    String toString() {
-      return '{checklist: ${this.checklist}, isDone: ${this.isDone}, user: $user}';
-    }
-  }
-
-
-
-
+}
--- lib/view/screens/grocery/my_grocery_lists.dart
+++ lib/view/screens/grocery/my_grocery_lists.dart
@@ -4,77 +4,88 @@ import 'package:flutterAcmFall/view/screens/grocery/checklistCreationScreen.dart
 import 'package:flutterAcmFall/view/screens/grocery/grocChecklistScreen.dart';
 import 'package:intl/intl.dart';
 
-class MyGroceryListsScreen extends StatefulWidget{
-
+class MyGroceryListsScreen extends StatefulWidget {
   _MyGroceryListsScreen createState() => _MyGroceryListsScreen();
-
-
 }
 
-class _MyGroceryListsScreen extends State<MyGroceryListsScreen>{
+class _MyGroceryListsScreen extends State<MyGroceryListsScreen> {
+  List<Grocery> myGroceryList = [];
+
+  void addToGroceryList(ChecklistModel cm) {
+    myGroceryList.add(Grocery(
+        checklist: cm, isDone: false, user: User(id: "A1", group: " ")));
+    print(myGroceryList.length);
+    print(myGroceryList);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-
-List<Grocery>myGroceryList=[];
-
-void addToGroceryList(ChecklistModel cm){
-  myGroceryList.add(Grocery(checklist:cm, isDone: false, user: User(id:"A1", group:" ")));
-  print(myGroceryList.length);
-  print(myGroceryList);
-  setState(() {
-
-  });
-
-
-}
-
     return Scaffold(
         backgroundColor: Colors.white,
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-
-          onPressed: (){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ChecklistCreationScreen(callback: addToGroceryList)));
-      },
-        ),
-
-      appBar: AppBar(
-        toolbarHeight: 80,
-        backgroundColor: Colors.white,
-        shadowColor: Colors.transparent,
-        title: Text("My Grocery List", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 30)),
-
-        actions: <Widget>[
-
-
-        FlatButton(
-          textColor: Color.fromRGBO(0, 108, 255, 1),
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => ChecklistCreationScreen(
+                    callback: addToGroceryList,
+                    checklistModel:
+                        ChecklistModel(date: DateTime.now(), items: []),
+                createNew: true)));
           },
-          child: Text("Back", style: new TextStyle(fontSize: 18, fontFamily:'SFProText',fontWeight: FontWeight.w700),
-
-          ))]),
-      body:ListView.builder(
-
-          itemCount: myGroceryList.length,
-          itemBuilder: (context, index) {
-            return CheckboxListTile(
-              title: Text(myGroceryList[index].checklist.items[0].text, style: TextStyle(fontFamily: 'SFProText', fontSize: 18)),
-              subtitle: Text(DateFormat('MM/dd/yyyy – kk:mm').format(myGroceryList[index].checklist.date), style: TextStyle(fontFamily: 'SFProText', fontSize: 14)),
-              value: myGroceryList[index].isDone,
-              controlAffinity: ListTileControlAffinity.leading,
-
-              onChanged: (bool value) {
-                setState(() {
-                  myGroceryList[index].isDone = value;
-                });
-              },
-            );
-          })
-
-
-    );
-   }
-
+        ),
+        appBar: AppBar(
+            toolbarHeight: 80,
+            backgroundColor: Colors.white,
+            shadowColor: Colors.transparent,
+            title: Text("My Grocery List",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30)),
+            actions: <Widget>[
+              FlatButton(
+                  textColor: Color.fromRGBO(0, 108, 255, 1),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    "Back",
+                    style: new TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'SFProText',
+                        fontWeight: FontWeight.w700),
+                  ))
+            ]),
+        body: ListView.builder(
+            itemCount: myGroceryList.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ChecklistCreationScreen(
+                            callback: addToGroceryList,
+                            checklistModel: myGroceryList[index].checklist,
+                        createNew: false)));
+                  },
+                  child: ListTile(
+                    leading: Checkbox(
+                      value: myGroceryList[index].isDone,
+                      onChanged: (bool value) {
+                        setState(() {
+                          myGroceryList[index].isDone = value;
+                        });
+                      },
+                    ),
+                    title: Text(myGroceryList[index].checklist.items[0].text,
+                        style:
+                            TextStyle(fontFamily: 'SFProText', fontSize: 18)),
+                    subtitle: Text(
+                        DateFormat('MM/dd/yyyy – kk:mm')
+                            .format(myGroceryList[index].checklist.date),
+                        style:
+                            TextStyle(fontFamily: 'SFProText', fontSize: 14)),
+                  ));
+            }));
+  }
 }
